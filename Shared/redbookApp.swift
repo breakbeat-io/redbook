cf5fa28208d56823eba67842f4074f80d2b337cc
Greg Hepworth <greg@breakbeat.io>
2020-10-29 13:41:57 +0000

    Initial Commit

---
+++ Shared/redbookApp.swift
@@ -0,0 +1,20 @@
+//
+//  redbookApp.swift
+//  Shared
+//
+//  Created by Greg Hepworth on 29/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct redbookApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
