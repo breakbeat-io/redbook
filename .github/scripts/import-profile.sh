f7ae9237a26dafe40feb1683ac49439d6346ebcb
Greg Hepworth <greg@breakbeat.io>
2020-11-28 23:19:44 +0000

    Adds some console logging to the profile import

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+echo "Making provisioning profile directory ..."
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+echo "Writing provisioning profile ..."
 echo "$PROVISIONING_PROFILE" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/redbook.mobileprovision
