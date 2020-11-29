ea9a1922688973085f99ae059276102f9cad1211
Greg Hepworth <greg@breakbeat.io>
2020-11-29 16:18:37 +0000

    Added a switch on branch to select the correct provisioing profile for the build

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -5,5 +5,19 @@ set -euo pipefail
 echo "Making provisioning profile directory ..."
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
+branch=$(git symbolic-ref --short -q HEAD)
+
+case $branch in
+"develop")
+    provisioningprofile=$PROVISIONING_PROFILE_ALPHA ;;
+"beta")
+    provisioningprofile=$PROVISIONING_PROFILE_BETA ;;
+"release")
+    provisioningprofile=$PROVISIONING_PROFILE_RELEASE ;;
+*)
+    echo "Not on a branch with a provisioing profile - exiting"
+    exit 1 ;;
+esac
+
 echo "Writing provisioning profile ..."
-echo "$PROVISIONING_PROFILE" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/redbook.mobileprovision
+echo "$provisioningprofile" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/redbook.mobileprovision
