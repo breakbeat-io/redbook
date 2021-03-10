97c27a56065fa64a0340720ef83916ef2d805045
Greg Hepworth <greg@breakbeat.io>
2021-03-10 13:53:20 +0000

    Updates to use newer version of AMTG that uses base64 key

--- redbook/Configuration/pre-actions/createAppleMusicAPIToken.sh
+++ redbook/Configuration/pre-actions/createAppleMusicAPIToken.sh
@@ -2,19 +2,19 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+# Delete any existing key/token FIRST so that if rest of script fails, app will not run (Xcode does not detect a failing pre-action).
+sed -i '' "/APPLE_MUSIC_API_TOKEN/d" $SRCROOT/redbook/Configuration/secrets.xcconfig
+
 echo "Generating Apple Music API Token."
 
 # Exit if any of the required variables are not available.
 # They are set in secrets.xcconfig and made available to this script by the Xcode pre-action bootstrapping.
 : "${APPLE_MUSIC_KEY_ID?Please set APPLE_MUSIC_KEY_ID in your secrets.xcconfig.}"
 : "${APPLE_DEVELOPER_TEAM_ID?Please set APPLE_DEVELOPER_TEAM_ID in your secrets.xcconfig.}"
-: "${APPLE_MUSIC_PRIVATE_KEY_PATH?Please set APPLE_MUSIC_PRIVATE_KEY_PATH in your secrets.xcconfig.}"
+: "${APPLE_MUSIC_PRIVATE_KEY?Please set APPLE_MUSIC_PRIVATE_KEY in your secrets.xcconfig.}"
 
 # Generate the token using the amtg helper [https://github.com/breakbeat-io/amtg]
-JWT=$($SCRIPTPATH/amtg -k "$APPLE_MUSIC_KEY_ID" -t "$APPLE_DEVELOPER_TEAM_ID" -p "$APPLE_MUSIC_PRIVATE_KEY_PATH")
-
-# Simply delete any existing key/token instead of fannying wtih fancy regex to update an existing value
-sed -i '' "/APPLE_MUSIC_API_TOKEN/d" $SRCROOT/redbook/Configuration/secrets.xcconfig
+JWT=$($SCRIPTPATH/amtg -k "$APPLE_MUSIC_PRIVATE_KEY" -i "$APPLE_MUSIC_KEY_ID" -t "$APPLE_DEVELOPER_TEAM_ID")
 
 # Add the generated token as a new line
 echo "APPLE_MUSIC_API_TOKEN = $JWT" >> $SRCROOT/redbook/Configuration/secrets.xcconfig
