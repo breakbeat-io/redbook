4f41c8109033fc8f49ffeb14d07266cdaa761f64
Greg Hepworth <greg@breakbeat.io>
2021-03-19 17:40:20 +0000

    Refactored the SearchBar to debounce in the view directly.

--- redbook/Search/SearchBar.swift
+++ redbook/Search/SearchBar.swift
@@ -10,20 +10,26 @@ import SwiftUI
 
 struct SearchBar: View {
   
-  @StateObject var viewModel: ViewModel
+  @State private var searchTerm: String = ""
+  
+  private let search: (String) -> Void
+  private let clear: () -> Void
+  
+  private var debouncerRelay = PassthroughSubject<String,Never>()
+  private var debouncer: AnyPublisher<String, Never>
   
   var body: some View {
     VStack {
       HStack {
         Text(Image(systemName: "magnifyingglass"))
-        TextField("Search albums", text: $viewModel.searchTerm)
+        TextField("Search albums", text: $searchTerm)
           .foregroundColor(.primary)
           .keyboardType(.webSearch)
         Button {
-          viewModel.searchTerm = ""
+          searchTerm = ""
         } label: {
           Text(Image(systemName: "xmark.circle.fill"))
-            .opacity(viewModel.searchTerm == "" ? 0 : 1)
+            .opacity(searchTerm == "" ? 0 : 1)
         }
       }
       .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
@@ -32,45 +38,22 @@ struct SearchBar: View {
       .cornerRadius(8.0)
       .padding([.top, .horizontal])
     }
+    .onChange(of: searchTerm, perform: { searchTerm in
+      searchTerm.isEmpty ? self.clear() : debouncerRelay.send(searchTerm)
+    })
+    .onReceive(debouncer, perform: { searchTerm in
+      search(searchTerm)
+    })
   }
   
   init(search: @escaping (String) -> Void, clear: @escaping () -> Void) {
-    let viewModel = ViewModel(search: search, clear: clear)
-    _viewModel = StateObject(wrappedValue: viewModel)
+    self.search = search
+    self.clear = clear
+    
+    self.debouncer = debouncerRelay
+      .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+      .removeDuplicates()
+      .eraseToAnyPublisher()
   }
   
-}
-
-extension SearchBar {
-  
-  class ViewModel: ObservableObject {
-
-    @Published var searchTerm: String = "" {
-      didSet {
-        searchTerm.isEmpty ? self.clear() : ()
-      }
-    }
-    private var searchTermSubscriber: Set<AnyCancellable> = []
-    private let search: (String) -> Void
-    private let clear: () -> Void
-    
-    init(search: @escaping (String) -> Void, clear: @escaping () -> Void) {
-      self.search = search
-      self.clear = clear
-      setupDebounceOnSearch()
-    }
-    
-    private func setupDebounceOnSearch() {
-      $searchTerm
-        .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
-        .removeDuplicates()
-        .filter{ !$0.isEmpty }
-        .compactMap{ $0 }
-        .sink(receiveValue: { searchTerm in
-          self.search(searchTerm)
-        })
-        .store(in: &searchTermSubscriber)
-    }
-    
-  }
 }
